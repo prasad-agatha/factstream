1d4a395e95ae0eb2e989367d11d807e8901f6f7e
prasad-agatha <prasad@agathainnovation.com>
2025-06-11 17:08:19 +0530

    Add files via upload

---
+++ components/elements/index.ts
@@ -0,0 +1,5 @@
+export {default as Link} from './Link';
+export {default as DocViewer} from './DocViewer';
+export {default as PdfViewer} from './PdfViewer';
+export {default as Spinner} from './Spinner';
+export {default as WindowScope} from './WindowScope';
